4e179905c3fa30a59829666f27f2f46506989802
Ali Tafakkori <alitafakkori82@gmail.com>
2024-12-15 18:55:03 +0330

    Create router.dart

---
+++ lib/router.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:software_testing_project_pfms/home.dart';
+import 'package:software_testing_project_pfms/login.dart';
+import 'package:software_testing_project_pfms/register.dart';
+
+enum Routes {
+  login,
+  register,
+  home,
+}
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  if (settings.name == Routes.login.toString() ||
+      settings.name == Navigator.defaultRouteName) {
+    return MaterialPageRoute(
+      builder: (context) {
+        return const Login();
+      },
+    );
+  } else if (settings.name == Routes.register.toString()) {
+    return CupertinoPageRoute(
+      builder: (context) {
+        return const Register();
+      },
+    );
+  } else if (settings.name == Routes.home.toString()) {
+    return CupertinoPageRoute(
+      builder: (context) {
+        return const Home();
+      },
+    );
+  }
+
+  return MaterialPageRoute(
+    builder: (context) => const Scaffold(),
+  );
+}
