24b7b30b045096873dd0951293d4d2bc71542171
Ali Tafakkori <alitafakkori82@gmail.com>
2024-12-15 19:19:11 +0330

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -10,7 +11,47 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+          child: SizedBox.expand(
+        child: Padding(
+          padding: const EdgeInsetsDirectional.only(
+            start: 24,
+            top: 80,
+            end: 24,
+            bottom: 40,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Icon(
+                CupertinoIcons.person_alt_circle,
+                color: Colors.amber,
+                size: 120,
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Container(
+                padding: const EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.black12,
+                ),
+                child: const Column(
+                  children: [
+                    TextField(),
+                    Divider(
+                      color: Colors.white,
+                    ),
+                    TextField(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      )),
+    );
   }
 }
-
