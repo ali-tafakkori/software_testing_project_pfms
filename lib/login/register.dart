37de45279f6e6e6f66d9543f11ec6df0842ff7c5
Ali Tafakkori <alitafakkori82@gmail.com>
2024-12-28 18:01:25 +0330

    Update register.dart

--- lib/login/register.dart
+++ lib/login/register.dart
@@ -25,6 +25,7 @@ class _RegisterState extends State<Register> {
   String? warning;
 
   AppProgressButtonState state = AppProgressButtonState.idle;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,204 +34,217 @@ class _RegisterState extends State<Register> {
       body: SingleChildScrollView(
         child: SafeArea(
             child: Padding(
-              padding: const EdgeInsetsDirectional.only(
-                start: 24,
-                top: 80,
-                end: 24,
-                bottom: 40,
+          padding: const EdgeInsetsDirectional.only(
+            start: 24,
+            top: 80,
+            end: 24,
+            bottom: 40,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Icon(
+                CupertinoIcons.person_add,
+                color: Colors.amber,
+                size: 90,
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Icon(
-                    CupertinoIcons.person_add,
-                    color: Colors.amber,
-                    size: 90,
-                  ),
-                  const SizedBox(
-                    height: 60,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.black.withAlpha(5),
-                      border: Border.all(
-                        color: hasFocus
-                            ? warning != null
+              const SizedBox(
+                height: 60,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black.withAlpha(5),
+                  border: Border.all(
+                    color: hasFocus
+                        ? warning != null
                             ? Colors.redAccent
                             : Colors.amber
-                            : Colors.transparent,
-                        width: 2,
-                      ),
-                    ),
-                    child: Column(
-                      children: [
-                        AppTextField(
-                          controller: _atfcName,
-                          inputType: TextInputType.text,
-                          hintText: "Name",
-                          noBorder: true,
-                          width: double.infinity,
-                          onChanged: (value) {
-                            setState(() {
-                              warning = null;
-                            });
-                          },
-                          onTap: () {
-                            setState(() {
-                              hasFocus = true;
-                            });
-                          },
-                        ),
-                        Container(
-                          height: 1,
-                          color: Colors.black26,
-                        ),
-                        AppTextField(
-                          controller: _atfcUser,
-                          inputType: TextInputType.text,
-                          hintText: "Username",
-                          noBorder: true,
-                          width: double.infinity,
-                          onChanged: (value) {
-                            setState(() {
-                              warning = null;
-                            });
-                          },
-                          onTap: () {
-                            setState(() {
-                              hasFocus = true;
-                            });
-                          },
-                        ),
-                        Container(
-                          height: 1,
-                          color: Colors.black26,
-                        ),
-                        AppTextField(
-                          obscure: obscurePass,
-                          controller: _atfcPass,
-                          inputType: TextInputType.text,
-                          noBorder: true,
-                          hintText: "Password",
-                          width: double.infinity,
-                          suffixIcon: _atfcPass.text.isNotEmpty
-                              ? IconButton(
-                            onPressed: () {
-                              setState(() {
-                                obscurePass = !obscurePass;
-                              });
-                            },
-                            icon: Icon(
-                              obscurePass
-                                  ? CupertinoIcons.eye
-                                  : CupertinoIcons.eye_slash,
-                              color: Colors.amber,
-                            ),
-                          )
-                              : null,
-                          onChanged: (value) {
-                            setState(() {
-                              warning = null;
-                            });
-                          },
-                          onTap: () {
-                            setState(() {
-                              hasFocus = true;
-                            });
-                          },
-                        ),
-                        Container(
-                          height: 1,
-                          color: Colors.black26,
-                        ),
-                        AppTextField(
-                          obscure: obscureRepeatPass,
-                          controller: _atfcRepeatPass,
-                          inputType: TextInputType.text,
-                          noBorder: true,
-                          hintText: "Repeat Password",
-                          width: double.infinity,
-                          suffixIcon: _atfcRepeatPass.text.isNotEmpty
-                              ? IconButton(
-                            onPressed: () {
-                              setState(() {
-                                obscureRepeatPass = !obscureRepeatPass;
-                              });
-                            },
-                            icon: Icon(
-                              obscureRepeatPass
-                                  ? CupertinoIcons.eye
-                                  : CupertinoIcons.eye_slash,
-                              color: Colors.amber,
-                            ),
-                          )
-                              : null,
-                          onChanged: (value) {
-                            setState(() {
-                              warning = null;
-                            });
-                          },
-                          onTap: () {
-                            setState(() {
-                              hasFocus = true;
-                            });
-                          },
-                        ),
-                      ],
-                    ),
+                        : Colors.transparent,
+                    width: 2,
                   ),
-                  if (warning != null)
-                    Container(
+                ),
+                child: Column(
+                  children: [
+                    AppTextField(
+                      controller: _atfcName,
+                      inputType: TextInputType.text,
+                      hintText: "Name",
+                      noBorder: true,
                       width: double.infinity,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 10,
-                        vertical: 8,
-                      ),
-                      child: Text(
-                        warning.toString(),
-                        style: const TextStyle(
-                          color: Colors.redAccent,
-                          fontWeight: FontWeight.w900,
-                        ),
-                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          warning = null;
+                        });
+                      },
+                      onTap: () {
+                        setState(() {
+                          hasFocus = true;
+                        });
+                      },
                     ),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  AppProgressButton(
-                    text: "Register",
-                    onPressed: onRegisterPressed,
-                    state: state,
-                    color:
-                    _atfcUser.text.isNotEmpty ? Colors.amber : Colors.blueGrey,
-                  ),
-                ],
+                    Container(
+                      height: 1,
+                      color: Colors.black26,
+                    ),
+                    AppTextField(
+                      controller: _atfcUser,
+                      inputType: TextInputType.text,
+                      hintText: "Username",
+                      noBorder: true,
+                      width: double.infinity,
+                      onChanged: (value) {
+                        setState(() {
+                          warning = null;
+                        });
+                      },
+                      onTap: () {
+                        setState(() {
+                          hasFocus = true;
+                        });
+                      },
+                    ),
+                    Container(
+                      height: 1,
+                      color: Colors.black26,
+                    ),
+                    AppTextField(
+                      obscure: obscurePass,
+                      controller: _atfcPass,
+                      inputType: TextInputType.text,
+                      noBorder: true,
+                      hintText: "Password",
+                      width: double.infinity,
+                      suffixIcon: _atfcPass.text.isNotEmpty
+                          ? IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  obscurePass = !obscurePass;
+                                });
+                              },
+                              icon: Icon(
+                                obscurePass
+                                    ? CupertinoIcons.eye
+                                    : CupertinoIcons.eye_slash,
+                                color: Colors.amber,
+                              ),
+                            )
+                          : null,
+                      onChanged: (value) {
+                        setState(() {
+                          warning = null;
+                        });
+                      },
+                      onTap: () {
+                        setState(() {
+                          hasFocus = true;
+                        });
+                      },
+                    ),
+                    Container(
+                      height: 1,
+                      color: Colors.black26,
+                    ),
+                    AppTextField(
+                      obscure: obscureRepeatPass,
+                      controller: _atfcRepeatPass,
+                      inputType: TextInputType.text,
+                      noBorder: true,
+                      hintText: "Repeat Password",
+                      width: double.infinity,
+                      suffixIcon: _atfcRepeatPass.text.isNotEmpty
+                          ? IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  obscureRepeatPass = !obscureRepeatPass;
+                                });
+                              },
+                              icon: Icon(
+                                obscureRepeatPass
+                                    ? CupertinoIcons.eye
+                                    : CupertinoIcons.eye_slash,
+                                color: Colors.amber,
+                              ),
+                            )
+                          : null,
+                      onChanged: (value) {
+                        setState(() {
+                          warning = null;
+                        });
+                      },
+                      onTap: () {
+                        setState(() {
+                          hasFocus = true;
+                        });
+                      },
+                    ),
+                  ],
+                ),
               ),
-            )),
+              if (warning != null)
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 8,
+                  ),
+                  child: Text(
+                    warning.toString(),
+                    style: const TextStyle(
+                      color: Colors.redAccent,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
+              const SizedBox(
+                height: 40,
+              ),
+              AppProgressButton(
+                text: "Register",
+                onPressed: onRegisterPressed,
+                state: state,
+                color:
+                    _atfcUser.text.isNotEmpty ? Colors.amber : Colors.blueGrey,
+              ),
+            ],
+          ),
+        )),
       ),
     );
   }
+
   void onRegisterPressed() async {
+    var name = _atfcName.text.trim();
     var username = _atfcUser.text.trim();
-    var pass = _atfcPass.text.trim();
-    if (username.isEmpty && pass.isEmpty) {
+    var password = _atfcPass.text.trim();
+    var repeatPassword = _atfcRepeatPass.text.trim();
+    FocusScope.of(context).children.forEach((FocusNode f) {
+      f.unfocus();
+    });
+    if (name.isEmpty) {
       hasFocus = true;
-      warning = "Username and Password are required.";
+      warning = "Name is required.";
+      _atfcName.requestFocus(context);
     } else if (username.isEmpty) {
       hasFocus = true;
       warning = "Username is required.";
-    } else if (pass.isEmpty) {
+      _atfcUser.requestFocus(context);
+    } else if (password.isEmpty) {
       hasFocus = true;
       warning = "Password is required.";
+      _atfcPass.requestFocus(context);
+    } else if (password != repeatPassword) {
+      hasFocus = true;
+      warning = "Repeat Password does not match Password.";
+      _atfcRepeatPass.requestFocus(context);
     } else {
       setState(() {
         state = AppProgressButtonState.loading;
         warning = null;
       });
       User? user =
-      await AppDatabase.instance.userDao.findByUserAndPass(username, pass);
+          await AppDatabase.instance.userDao.findByUsername(username);
       if (user != null) {
       } else {
         hasFocus = true;
@@ -240,6 +254,4 @@ class _RegisterState extends State<Register> {
     }
     setState(() {});
   }
-
 }
-
