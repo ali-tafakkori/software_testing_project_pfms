96fc31ed327a85eaf092668cc019dc6ca8c41efe
Mojtaba Tafakkori | مجتبی تفکری <alitafakkori82@gmail.com>
2024-12-30 02:05:51 +0330

    Update app_database.dart

--- lib/db/app_database.dart
+++ lib/db/app_database.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'package:floor/floor.dart';
 import 'package:software_testing_project_pfms/db/customer_dao.dart';
+import 'package:software_testing_project_pfms/db/invoice_dao.dart';
 import 'package:software_testing_project_pfms/db/user_dao.dart';
 import 'package:software_testing_project_pfms/models/customer.dart';
+import 'package:software_testing_project_pfms/models/invoice.dart';
 import 'package:software_testing_project_pfms/models/user.dart';
 
 import 'package:sqflite/sqflite.dart' as sqflite;
@@ -12,10 +14,15 @@ part 'app_database.g.dart';
 @Database(version: 1, entities: [
   User,
   Customer,
+  Invoice,
 ])
 abstract class AppDatabase extends FloorDatabase {
   UserDao get userDao;
+
   CustomerDao get customerDao;
+
+  InvoiceDao get invoiceDao;
+
   static late AppDatabase _appDatabase;
 
   static AppDatabase get instance {
@@ -27,5 +34,4 @@ abstract class AppDatabase extends FloorDatabase {
   static Future<void> init() async {
     _appDatabase = await $FloorAppDatabase.databaseBuilder(_dbName).build();
   }
-
 }
