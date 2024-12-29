bd821e92cdd7ac642db3f518cac04700efa4a8ba
Ali Tafakkori <alitafakkori82@gmail.com>
2024-12-29 17:57:36 +0330

    Update customer_dao.dart

--- lib/db/customer_dao.dart
+++ lib/db/customer_dao.dart
@@ -3,12 +3,16 @@ import 'package:software_testing_project_pfms/models/customer.dart';
 
 @floor.dao
 abstract class CustomerDao {
+  @floor.Query('SELECT * FROM customer')
+  Future<List<Customer>> findAll();
+  @floor.Query('SELECT * FROM user WHERE id = :id')
+  Future<Customer?> findById(int id);
   @floor.insert
   Future<void> insert(Customer customer);
 
   @floor.update
   Future<void> update(Customer customer);
 
-  @floor.delete
-  Future<void> delete(Customer customer);
+  @floor.Query('DELETE * FROM customer WHERE id = :id')
+  Future<void> deleteById(int id);
 }
