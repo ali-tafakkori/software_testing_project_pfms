6bd2672aeae771d3ba697b8321cee1eca5c7ee55
Ali Tafakkori <alitafakkori82@gmail.com>
2025-01-10 20:01:58 +0330

    Update home.dart

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -25,62 +25,8 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: <Widget>[
         const Dashboard(),
-        Card(
-          shadowColor: Colors.transparent,
-          margin: const EdgeInsets.all(8.0),
-          child: SizedBox.expand(
-            child: Center(
-              child: Text(
-                'Home page',
-                style: theme.textTheme.titleLarge,
-              ),
-            ),
-          ),
-        ),
         const Invoices(),
         const Customers(),
-
-        /// Messages page
-        ListView.builder(
-          reverse: true,
-          itemCount: 2,
-          itemBuilder: (BuildContext context, int index) {
-            if (index == 0) {
-              return Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  margin: const EdgeInsets.all(8.0),
-                  padding: const EdgeInsets.all(8.0),
-                  decoration: BoxDecoration(
-                    color: theme.colorScheme.primary,
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: Text(
-                    'Hello',
-                    style: theme.textTheme.bodyLarge!
-                        .copyWith(color: theme.colorScheme.onPrimary),
-                  ),
-                ),
-              );
-            }
-            return Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                margin: const EdgeInsets.all(8.0),
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: theme.colorScheme.primary,
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Text(
-                  'Hi!',
-                  style: theme.textTheme.bodyLarge!
-                      .copyWith(color: theme.colorScheme.onPrimary),
-                ),
-              ),
-            );
-          },
-        ),
       ][currentPageIndex],
       bottomNavigationBar: NavigationBar(
         onDestinationSelected: (int index) {
@@ -94,10 +40,6 @@ class _HomeState extends State<Home> {
           NavigationDestination(
             icon: Icon(CupertinoIcons.home),
             label: 'Dashboard',
-          ),
-          NavigationDestination(
-            icon: Icon(CupertinoIcons.square_list),
-            label: 'Reports',
           ),
           NavigationDestination(
             icon: Icon(CupertinoIcons.list_bullet_below_rectangle),
