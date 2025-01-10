ae9ccf50c640c53b62c92253b045ff5a08f2b26e
Ali Tafakkori <alitafakkori82@gmail.com>
2025-01-10 15:43:07 +0330

    Update customer.dart

--- lib/models/customer.dart
+++ lib/models/customer.dart
@@ -6,10 +6,12 @@ class Customer {
   final int? id;
   final String name;
   final int balance;
+  final int userId;
 
   const Customer({
     this.id = -1,
     required this.name,
     this.balance = 0,
+    required this.userId,
   });
 }
