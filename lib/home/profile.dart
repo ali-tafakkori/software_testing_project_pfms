26da576ff63c5f7253ffa4927f2d5f4c4857188a
Ali Tafakkori <alitafakkori82@gmail.com>
2025-01-11 16:24:30 +0330

    Update profile.dart

--- lib/home/profile.dart
+++ lib/home/profile.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:software_testing_project_pfms/db/app_database.dart';
+import 'package:software_testing_project_pfms/main.dart';
+import 'package:software_testing_project_pfms/models/user.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -10,6 +15,79 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox.expand(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).padding.top + 24,
+                bottom: 16,
+              ),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(
+                    24,
+                  ),
+                ),
+                color: Colors.amber,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(
+                      14,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white.withAlpha(125),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: const Icon(
+                      CupertinoIcons.person,
+                      color: Colors.black,
+                      size: 60,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  FutureBuilder<User?>(
+                    future: AppDatabase.instance.userDao
+                        .findById(MyApp.of(context)!.userId!),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              snapshot.data!.name,
+                              style: const TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Text(
+                              snapshot.data!.username,
+                              style: const TextStyle(
+                                color: Colors.white54,
+                              ),
+                            ),
+                          ],
+                        );
+                      }
+                      return LoadingAnimationWidget.newtonCradle(
+                        color: Colors.black,
+                        size: 53,
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
