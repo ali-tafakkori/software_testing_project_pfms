68e0e68455abfd46083233b76e5276042a8aa9d5
Ali Tafakkori <alitafakkori82@gmail.com>
2025-01-11 18:01:09 +0330

    Update user_dao.dart

--- lib/db/user_dao.dart
+++ lib/db/user_dao.dart
@@ -6,10 +6,17 @@ abstract class UserDao {
   @floor.insert
   Future<void> insert(User user);
 
-  @floor.update
-  Future<void> update(User user);
+  @floor.Query('UPDATE user SET name = :name WHERE id = :id')
+  Future<void> updateNameById(String name, int id);
 
-  @floor.Query('SELECT * FROM user WHERE username = :username AND password = :password')
+  @floor.Query('UPDATE user SET username = :username WHERE id = :id')
+  Future<void> updateUsernameById(String username, int id);
+
+  @floor.Query('UPDATE user SET password = :password WHERE id = :id')
+  Future<void> updatePasswordById(String password, int id);
+
+  @floor.Query(
+      'SELECT * FROM user WHERE username = :username AND password = :password')
   Future<User?> findByUsernameAndPassword(String username, String password);
 
   @floor.Query('SELECT * FROM user WHERE username = :username')
