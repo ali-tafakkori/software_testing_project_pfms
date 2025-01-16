280f0dfcffe27a9a319cac498a8b12445da26d43
Mojtaba Tafakkori | مجتبی تفکری <alitafakkori82@gmail.com>
2025-01-17 03:13:11 +0330

    Update invoice_details.dart

--- lib/home/invoice_details.dart
+++ lib/home/invoice_details.dart
@@ -1,6 +1,10 @@
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
-import 'package:software_testing_project_pfms/models/customer.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:software_testing_project_pfms/db/app_database.dart';
+import 'package:software_testing_project_pfms/models/invoice.dart';
 
 class InvoiceDetails extends StatefulWidget {
   final int id;
@@ -15,20 +19,57 @@ class InvoiceDetails extends StatefulWidget {
 }
 
 class _InvoiceDetailsState extends State<InvoiceDetails> {
-  Customer? customer;
+  Invoice? invoice;
+
+  @override
+  void initState() {
+    get();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (customer != null) {}
+    if (invoice != null) {
+      return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: ListTile(
+            subtitle: Text(DateFormat("yyyy/MM/dd")
+                .format(invoice!.dateTime)),
+            title: Text(
+              NumberFormat.simpleCurrency(decimalDigits: 0)
+                  .format(invoice!.amount),
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ),
+        body: PhotoView(
+          //TODO:
+          imageProvider: NetworkImage(
+              "https://design-assets.adobeprojectm.com/content/download/express/public/urn:aaid:sc:VA6C2:ca78dfb7-b478-548f-8f49-17a0d719f24c/component?assetType=TEMPLATE&etag=27cd1224f83b4f24bb56476616474b63&revision=215141d1-4747-4b2d-a8c1-dc7664031690&component_id=d640bea6-cfa0-4e68-950f-07fbd5942d3d"),
+        ),
+      );
+    }
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(),
       body: Center(
-        child: LoadingAnimationWidget.newtonCradle(
-          color: Colors.black,
-          size: 53,
+        child: LoadingAnimationWidget.inkDrop(
+          color: Colors.amber,
+          size: 48,
         ),
       ),
     );
+  }
+
+  void get() async {
+    invoice = await AppDatabase.instance.invoiceDao.findById(
+      widget.id,
+    );
+    setState(() {
+
+    });
   }
 }
