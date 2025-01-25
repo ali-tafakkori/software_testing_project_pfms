a6fcfc68d5cab7f95f4b67d53c578ebc0584b636
Mojtaba Tafakkori | مجتبی تفکری <alitafakkori82@gmail.com>
2025-01-25 09:42:47 +0330

    Create charge_dao.dart

---
+++ lib/db/charge_dao.dart
@@ -0,0 +1,23 @@
+import 'package:floor/floor.dart' as floor;
+import 'package:software_testing_project_pfms/models/charge.dart';
+
+@floor.dao
+abstract class ChargeDao {
+  @floor.Query('SELECT * FROM charge WHERE customerId = :customerId')
+  Future<List<Charge>> findByCustomerId(int customerId);
+
+  @floor.Query('SELECT * FROM charge WHERE id = :id')
+  Future<Charge?> findById(int id);
+
+  @floor.insert
+  Future<void> insert(Charge charge);
+
+  @floor.update
+  Future<void> update(Charge charge);
+
+  @floor.Query('DELETE FROM charge WHERE id = :id')
+  Future<void> deleteById(int id);
+
+  @floor.Query('SELECT COUNT(*) FROM charge WHERE customerId = :customerId')
+  Future<int?> countByCustomerId(int customerId);
+}
