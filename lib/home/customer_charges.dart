a13b0edcfee6d0794179513ebce9599ac5e5684c
Mojtaba Tafakkori | مجتبی تفکری <alitafakkori82@gmail.com>
2025-01-26 01:12:56 +0330

    Update customer_charges.dart

--- lib/home/customer_charges.dart
+++ lib/home/customer_charges.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:software_testing_project_pfms/db/app_database.dart';
+import 'package:software_testing_project_pfms/home/charge_dialog.dart';
 import 'package:software_testing_project_pfms/models/charge.dart';
-import 'package:software_testing_project_pfms/router.dart';
 import 'package:software_testing_project_pfms/utils.dart';
 
 class CustomerCharges extends StatefulWidget {
@@ -22,6 +22,29 @@ class _CustomerChargesState extends State<CustomerCharges> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(
+          Icons.add_rounded,
+        ),
+        onPressed: () {
+          ChargeDialog.show(
+            context,
+            widget.id,
+          ).then(
+            (value) async {
+              if (value != null) {
+                await AppDatabase.instance.chargeDao.insert(value);
+                await AppDatabase.instance.customerDao.chargeBalanceById(
+                  value.amount,
+                  widget.id,
+                );
+                setState(() {});
+              }
+            },
+          );
+        },
+      ),
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: FutureBuilder(
@@ -46,82 +69,100 @@ class _CustomerChargesState extends State<CustomerCharges> {
           },
         ),
       ),
-      body: FutureBuilder(
-          future: AppDatabase.instance.chargeDao.findByCustomerId(widget.id),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              var chargeList = snapshot.data!;
-              if (chargeList.isEmpty) {
-                return const Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                      Icons.folder_off_rounded,
-                      size: 80,
-                      color: Colors.black45,
-                    ),
-                    Text(
-                      "بدون تاریخچه شارژر",
-                      style: TextStyle(
-                        fontSize: 17,
-                        fontWeight: FontWeight.w900,
-                        color: Colors.black45,
-                      ),
-                    ),
-                  ],
-                );
-              } else {
-                return ListView.builder(
-                  itemCount: chargeList.length,
-                  itemBuilder: (context, i) {
-                    Charge charge = chargeList[i];
-                    return Card(
-                      child: ListTile(
-                        leading: const Icon(
-                          Icons.person,
-                          color: Colors.amber,
-                          size: 30,
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: FutureBuilder(
+            future: AppDatabase.instance.chargeDao.findByCustomerId(widget.id),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var chargeList = snapshot.data!;
+                if (chargeList.isEmpty) {
+                  return const SizedBox.expand(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.folder_off_rounded,
+                          size: 80,
+                          color: Colors.black45,
                         ),
-                        title: Text(
-                          "${charge.amount > 0 ? "+" : ""} ${NumberFormat.simpleCurrency(decimalDigits: 0).format(charge.amount)}",
+                        Text(
+                          "بدون تاریخچه شارژر",
                           style: TextStyle(
-                            color: charge.amount >= 0
-                                ? charge.amount == 0
-                                    ? null
-                                    : Colors.green
-                                : Colors.redAccent,
-                            fontWeight: FontWeight.w600,
+                            fontSize: 17,
+                            fontWeight: FontWeight.w900,
+                            color: Colors.black45,
                           ),
                         ),
-                        subtitle: Text(formatCompactDate(charge.dateTime)),
-                        trailing: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            IconButton(
-                              onPressed: () async {
-                                await AppDatabase.instance.chargeDao
-                                    .deleteById(charge.id!);
-                                setState(() {});
-                              },
-                              icon: const Icon(
-                                Icons.delete_outline,
-                              ),
+                      ],
+                    ),
+                  );
+                } else {
+                  return ListView.builder(
+                    itemCount: chargeList.length,
+                    itemBuilder: (context, i) {
+                      Charge charge = chargeList[i];
+                      return Card(
+                        color: charge.amount >= 0
+                            ? charge.amount == 0
+                                ? null
+                                : Colors.green
+                            : Colors.redAccent,
+                        child: ListTile(
+                          leading: Icon(
+                            charge.amount >= 0
+                                ? charge.amount == 0
+                                    ? null
+                                    : Icons.add_circle_outline_rounded
+                                : Icons.remove_circle_outline_rounded,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                          title: Text(
+                            NumberFormat.simpleCurrency(decimalDigits: 0)
+                                .format(charge.amount.abs()),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
                             ),
-                          ],
+                          ),
+                          trailing: Text(
+                            formatCompactDate(charge.dateTime),
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          /*trailing: IconButton(
+                            onPressed: () async {
+                              await AppDatabase.instance.chargeDao
+                                  .deleteById(charge.id!);
+                              await AppDatabase.instance.customerDao
+                                  .chargeBalanceById(
+                                charge.amount * -1,
+                                widget.id,
+                              );
+                              setState(() {});
+                            },
+                            icon: const Icon(
+                              Icons.delete_outline,
+                              color: Colors.white,
+                            ),
+                          ),*/
                         ),
-                      ),
-                    );
-                  },
-                );
+                      );
+                    },
+                  );
+                }
               }
-            }
-            return Center(
-              child: LoadingAnimationWidget.inkDrop(
-                color: Colors.amber,
-                size: 48,
-              ),
-            );
-          }),
+              return Center(
+                child: LoadingAnimationWidget.inkDrop(
+                  color: Colors.amber,
+                  size: 48,
+                ),
+              );
+            }),
+      ),
     );
   }
 }
